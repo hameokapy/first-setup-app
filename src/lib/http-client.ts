fb2a8a6d6ebe846f7c397c4b1777628772302939
Yoshi <hameo.kapy@gmail.com>
2026-08-19 09:00:31 +0700

    feat: config axios get call

--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
 
 import { handleSessionError } from '@/lib/handle-server-error'
 
@@ -19,3 +19,28 @@ http.interceptors.response.use(
     return Promise.reject(error)
   }
 )
+
+export const axiosClient = {
+  get: async (
+    endpoint: string,
+    queryParams: Record<string, string>,
+    params: Record<string, unknown>,
+    config: AxiosRequestConfig
+  ) => {
+    const url = generateCompletedEndpoint(endpoint, params)
+
+    const response = await http.get(url, {
+      params: {
+        queryParams
+      },
+      ...config
+    })
+    return response.data
+  },
+}
+
+function generateCompletedEndpoint(endpoint: string, params: Record<string, unknown>): string {
+  return endpoint.replace(/:([a-zA-Z0-9]+)/g, (_, key) => {
+    return params[key] !== undefined ? String(params[key]) : `:${key}`
+  })
+}
